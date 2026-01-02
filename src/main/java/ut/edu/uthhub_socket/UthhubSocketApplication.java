b2c3b431b1e20aea5b72dd7d6ce98d2a89ead885
tienvybui05 <tienvy27052005@gmail.com>
2026-01-02 21:51:14 +0700

    Init

---
+++ src/main/java/ut/edu/uthhub_socket/UthhubSocketApplication.java
@@ -0,0 +1,13 @@
+package ut.edu.uthhub_socket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UthhubSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UthhubSocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ut/edu/uthhub_socket/UthhubSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package ut.edu.uthhub_socket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UthhubSocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
